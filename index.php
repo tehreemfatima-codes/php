f07c14cc29379a3ec6f4c8e71b76b7b4e1728e52
tehreem fatima <163853253+tehreemfatima-codes@users.noreply.github.com>
2024-08-01 00:24:14 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+  <section>
+    <h2>Introduction</h2>
+    <p>Assalam o Alaikum,this is <?php echo "Tehreem Fatima"; ?> & i am currently doing diploma in software engineering</p>
+  </section>  
+  <section>
+    <h2>Islamic Months</h2>
+    <ol>
+        <li>Moharram</li>
+        <li>Safar</li>
+        <li>Rabi ul Awwal</li>
+        <li>Rabi us Sani</li>
+        <li>Jumad ul Awwal</li>
+        <li>Jumad us Sani</li>
+        <li>Rajab</li>
+        <li>Shaban</li>
+        <li>Ramadan</li>
+        <li>Shawwal</li>
+        <li>Dhu al Qadah</li>
+        <li>Dhu al Hijjah</li>
+    </ol>
+  </section>
+</body>
+</html>
